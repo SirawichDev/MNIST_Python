77b199d4a291e0378e0cc53894f3787b511a8027
Ex <sirawit0676@gmail.com>
2017-09-28 11:50:13 +0700

    for loop

--- mnist_code.py
+++ mnist_code.py
@@ -7,8 +7,9 @@ x_point = []
 y_point = []
 a = 0.22
 b = 0.78
-x = np.random.normal(0.0, 0.5)
-y = a * x + b + np.random.normal(0.0, 0.1)
+for i in range(number_of_points):
+    x = np.random.normal(0.0, 0.5)
+    y = a * x + b + np.random.normal(0.0, 0.1)
 
 cost_function = tf.reduce_mean(tf.square(y - y_point))
 optimizer = tf.train.GradientDescentOptimizer(0.5)
