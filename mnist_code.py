8291b0f8be946f9ebb18594bf31d94d86df084ea
Ex <sirawit0676@gmail.com>
2017-09-28 11:57:02 +0700

    tensorflow.mnist

--- mnist_code.py
+++ mnist_code.py
@@ -1,20 +1,9 @@
-import get_data
+from tensorflow.examples.tutorials.mnist import input_data
 import numpy as np
 import matplotlib.pyplot as plt
 import tensorflow as tf
-number_of_points = 200
-x_point = []
-y_point = []
-a = 0.22
-b = 0.78
-for i in range(number_of_points):
-    x = np.random.normal(0.0, 0.5)
-    y = a * x + b + np.random.normal(0.0, 0.1)
 
-cost_function = tf.reduce_mean(tf.square(y - y_point))
-optimizer = tf.train.GradientDescentOptimizer(0.5)
-train = optimizer.minimize(cost_function)
-mnist_images = get_data.read_data_sets("MNIST_data/", one_hot=False)
+mnist_images = input_data.read_data_sets("MNIST_data/", one_hot=False)
 train.next_batch(10)
 pixels,real_values = mnist_images.train.next_batch(10)
 print("list of values loaded ", real_values)
